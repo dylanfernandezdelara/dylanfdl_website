cc224571e9119244deb475c73edd783533f4a8f3
dylanfernandezdelara <fernandezdelaradylan@gmail.com>
2026-02-27 09:18:42 -0800

    chore: add tailwind and shadcn infrastructure

---
+++ tailwind.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  darkMode: ['class'],
+  content: [
+    './app/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './lib/**/*.{ts,tsx}',
+    './content/**/*.{md,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('tailwindcss-animate')],
+}
+
+export default config
